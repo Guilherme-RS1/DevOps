6de6d4be1beeef9c04590454d8581807b7896b47
Guilherme <guirs2602@gmail.com>
2025-04-15 14:44:51 -0300

    adicionado função teste no arquivo main.py & criado pasta teste com o arquivo teste.txt

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello":"World"}
+
+@app.get("/funcaoteste")
+async def funcaoteste(): 
+    return {
+        "teste": True,
+        "num_aleatorio": random.randint(0, 10000)
+        }
